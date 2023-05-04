b7c5c9609cee55dc8a1417b8540659358abf803b
Shadow1900 <118683823+Shadow1900@users.noreply.github.com>
2023-05-04 10:40:29 -0600

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { CapitalizadoPipe } from './capitalizado.pipe';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, CapitalizadoPipe],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
