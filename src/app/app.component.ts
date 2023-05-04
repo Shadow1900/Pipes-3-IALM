d8e5ddae9e959d6c6dbcf8f6875059ad2b04125e
Shadow1900 <118683823+Shadow1900@users.noreply.github.com>
2023-05-04 10:38:40 -0600

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  nombre = 'jUAN carlos rodriguez mares';
+  nombre = 'ivan alejandro lopez martinez';
 }
